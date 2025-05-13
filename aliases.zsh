6adb8f095e621f47320eb947e6fc63c8fafaf8e8
girign <giridhargnair93@gmail.com>
2025-05-13 07:52:10 +0530

    some useful aliases

---
+++ aliases.zsh
@@ -0,0 +1,21 @@
+# git aliases
+
+alias ga='git add'
+alias gs='git status'
+alias gch='git checkout'
+alias gcom='git commit'
+alias gb='git branch'
+alias gpsho='git push origin'
+alias gplo='git pull origin'
+alias grb='git rebase'
+alias gd='git diff'
+alias glog='git log --graph --oneline --decorate'
+alias gst='git stash'
+alias gpop='git stash pop'
+alias gme='git merge'
+
+# npm aliases
+alias npb='npm run build'
+alias npd='npm run dev'
+alias npi='npm install'
+alias nps='npm start'
